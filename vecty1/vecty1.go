01b34ea49ff31e07d7e85c28294792ab8c410332
Guillaume Leroi <leroi.g@gmail.com>
2017-07-26 23:04:24 +0200

    vecty1: initial html&css

--- vecty1/vecty1.go
+++ vecty1/vecty1.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gopherjs/vecty"
 	"github.com/gopherjs/vecty/elem"
 	"github.com/gopherjs/vecty/event"
+	"github.com/gopherjs/vecty/prop"
 )
 
 type Main struct {
@@ -34,10 +35,15 @@ func (m *Main) Remove(e *vecty.Event) {
 func (m *Main) Render() *vecty.HTML {
 	return elem.Body(elem.Div(
 		elem.Paragraph(
+			prop.Class("counter"),
 			vecty.Text(fmt.Sprintf("Counter : %d", m.counter)),
 		),
-		elem.Button(vecty.Text("ADD"), event.Click(m.Add)),
-		elem.Button(vecty.Text("REMOVE"), event.Click(m.Remove)),
+		elem.Button(
+			prop.Class("action"),
+			vecty.Text("Add"), event.Click(m.Add)),
+		elem.Button(
+			prop.Class("action"),
+			vecty.Text("Remove"), event.Click(m.Remove)),
 	))
 }
 
